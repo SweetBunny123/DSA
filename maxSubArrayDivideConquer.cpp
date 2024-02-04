de8e3d5c0d19537f533c5d820b1a2fc66c912418
SweetBunny123 <143780392+SweetBunny123@users.noreply.github.com>
2024-02-04 16:45:20 +0000

    again

--- maxSubArrayDivideConquer.cpp
+++ maxSubArrayDivideConquer.cpp
@@ -1,41 +1,41 @@
-#include<bits/stdc++.h>
-int maxcrossingarraysum(vector<int> &arr, int low, int mid, int high)
-{
-  maxr = INT_MIN;
-  maxl = INT_MIN;
-  for(int i = mid; i<high; i++)
-    {
-      rsum+=arr[i];
-      maxr= max(maxr,rsum);
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxcrossingarraysum(vector<int>& arr, int low, int mid, int high) {
+    int maxr = INT_MIN;
+    int maxl = INT_MIN;
+    int rsum = 0, lsum = 0;
+
+    for (int i = mid; i >= low; i--) {
+        lsum += arr[i];
+        maxl = max(maxl, lsum);
     }
-  for(int j = mid-1; j>=0; j--)
-    {
-      lsum+=arr[j];
-      maxl = max(maxl,lsum);
+
+    rsum = 0;
+    for (int i = mid + 1; i <= high; i++) {
+        rsum += arr[i];
+        maxr = max(maxr, rsum);
     }
-  return maxl+maxr;
+
+    return maxl + maxr;
 }
 
-int maxarraysum(vector<int>&arr, int l, int r)
-{
-  if(l==r)
-  {
-    return arr[l];
-  }
-  else if(l>r)
-  {
-    return 0;
-  }
-  else
-  {
-    int mid = l+(r-l)/2;
-    int left = maxarraysum(arr,l,mid);
-    int right = maxarraysum(arr,mid+1,r);
-    int center = maxcrossingarraysum(arr, l,mid,r);
-    return max(left,max(right,center));
-  }
+int maxarraysum(vector<int>& arr, int l, int r) {
+    if (l == r) {
+        return arr[l];
+    } else {
+        int mid = l + (r - l) / 2;
+        int left = maxarraysum(arr, l, mid);
+        int right = maxarraysum(arr, mid + 1, r);
+        int center = maxcrossingarraysum(arr, l, mid, r);
+        return max(max(left, right), center);
+    }
 }
-int main()
-{
-  
+
+int main() {
+    vector<int> arr = {1, -3, 4, -2, -1, 6};
+    int n = arr.size();
+    int max_sum = maxarraysum(arr, 0, n - 1);
+    cout << "Maximum subarray sum is " << max_sum << endl;
+    return 0;
 }
