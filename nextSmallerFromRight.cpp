8b01e2aa0986bc2fdbd8aef493d1a654b7acb5cb
Prakrititz Borah <143780392+SweetBunny123@users.noreply.github.com>
2024-02-23 14:54:49 +0000

    hh

---
+++ nextSmallerFromRight.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std; 
+vector<int>nextSmaller(vector<int>&nums)
+{
+  stack<int>s;
+  vector<int>ans(nums.size(),-1);
+  for(int i = nums.size()-1; i>=0; i--)
+    {
+      while(!s.empty() && s.top()>=nums[i])
+        {
+          s.pop();
+        }
+      if(!s.empty())
+      {
+        ans[i] = s.top();
+        
+      }
+      s.push(nums[i]);
+    }
+  return ans;
+}
+int main()
+{
+  int n; 
+  cin>>n;
+  vector<int>arr(n);
+  for(int i = 0; i<n; i++)
+    {
+      cin>>arr[i];
+    }
+  vector<int>ans = nextSmaller(arr);
+  for(int i = 0; i<n; i++)
+    {
+      cout<<ans[i]<<" ";
+      
+    }
+  cout<<endl;
+  return 0; 
+}
